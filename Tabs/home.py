a18404c19af5cbf8c76c89ddb199ec01fac13fb4
manavsudhir <117347171+manavsudhir@users.noreply.github.com>
2023-10-13 23:53:22 +0530

    Update home.py

--- Tabs/home.py
+++ Tabs/home.py
@@ -7,7 +7,7 @@ def app():
     """This function create the home page"""
     
     # Add title to the home page
-    st.title("Harvest Quantity Estimation System")
+    st.title("Minor project   crop yeild prediction")
 
     # Add image to the home page
     st.image("./images/home.png")
